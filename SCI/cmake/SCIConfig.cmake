032ec27d7e1b3e6e2748661fb91315b7493c0736
alarst13 <mehrkisha8@gmail.com>
2025-09-05 19:06:21 -0400

    removed submodules

--- SCI/cmake/SCIConfig.cmake
+++ SCI/cmake/SCIConfig.cmake
@@ -34,8 +34,8 @@ find_dependency(GMP REQUIRED)
 find_dependency(Threads REQUIRED)
 
 set_and_check(INSTALL_DIR "${PACKAGE_PREFIX_DIR}")
-find_dependency(Eigen3 REQUIRED PATHS "/home/ala22014/cryptoLp/SCI/build/../extern/eigen/build" NO_DEFAULT_PATH)
-find_dependency(SEAL REQUIRED PATHS "/home/ala22014/cryptoLp/SCI/build" NO_DEFAULT_PATH)
+find_dependency(Eigen3 REQUIRED PATHS "/home/ala22014/github/cryptoLp/SCI/build/../extern/eigen/build" NO_DEFAULT_PATH)
+find_dependency(SEAL REQUIRED PATHS "/home/ala22014/github/cryptoLp/SCI/build" NO_DEFAULT_PATH)
 find_dependency(OpenMP REQUIRED)
 
 # Add the targets file
--- build.sh
+++ build.sh
@@ -1,19 +1,52 @@
 #!/bin/bash
 
-# Add and initialize SEAL submodule
-echo "Adding SEAL submodule..."
-git submodule add https://github.com/microsoft/SEAL.git SCI/extern/SEAL
-pushd SCI/extern/SEAL && git checkout 1d5c816 && popd
+set -e  # Stop on first error
 
-# Add and initialize Eigen submodule if missing
-echo "Adding Eigen submodule..."
-git submodule add https://gitlab.com/libeigen/eigen.git SCI/extern/eigen
-pushd SCI/extern/eigen && git checkout 603e213 && popd
+# --------- CONFIG ---------
+SEAL_COMMIT=1d5c816
+EIGEN_COMMIT=603e213
+SEAL_DIR=SCI/extern/SEAL
+EIGEN_DIR=SCI/extern/eigen
+# --------------------------
 
-# Update all submodules recursively
-git submodule update --init --recursive
+# Ensure extern directory exists
+mkdir -p SCI/extern
 
+clone_if_missing() {
+    local dir=$1
+    local repo=$2
+    local commit=$3
+
+    if [ ! -d "$dir/.git" ]; then
+        echo "Cloning $repo into $dir..."
+        git clone "$repo" "$dir"
+        pushd "$dir"
+        git checkout "$commit"
+        popd
+    else
+        echo "Repo already cloned: $dir"
+    fi
+}
+
+clone_if_missing "$SEAL_DIR" https://github.com/microsoft/SEAL.git "$SEAL_COMMIT"
+clone_if_missing "$EIGEN_DIR" https://gitlab.com/libeigen/eigen.git "$EIGEN_COMMIT"
+
+# --------- OPENSSL CONFIG ---------
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    if ! command -v brew &>/dev/null; then
+        echo "Homebrew not found. Please install OpenSSL manually or install Homebrew."
+        exit 1
+    fi
+    OPENSSL_ROOT=$(brew --prefix openssl@3)
+    OPENSSL_FLAGS="-DOPENSSL_ROOT_DIR=$OPENSSL_ROOT -DOPENSSL_INCLUDE_DIR=$OPENSSL_ROOT/include -DOPENSSL_CRYPTO_LIBRARY=$OPENSSL_ROOT/lib"
+else
+    OPENSSL_FLAGS=""
+fi
+# ----------------------------------
+
+# Build
 mkdir -p build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=./install ..
+
+cmake .. -DCMAKE_INSTALL_PREFIX=./install $OPENSSL_FLAGS
 cmake --build . --target install --parallel

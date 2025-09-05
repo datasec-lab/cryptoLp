dbc1aeec3a443eb183a242fda8d656649cbe3eac
Ali Arastehfard <alarst13@Alis-MacBook-Pro.local>
2025-09-05 18:43:54 -0400

    submodules fixed

--- build.sh
+++ build.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
-# Add and initialize SEAL submodule if missing
-if [ ! -d "SCI/extern/SEAL" ]; then
-    echo "Adding SEAL submodule..."
-    git submodule add https://github.com/microsoft/SEAL.git SCI/extern/SEAL
-    pushd SCI/extern/SEAL && git checkout 1d5c816 && popd
-else
-    echo "SEAL submodule already exists."
-fi
+# Add and initialize SEAL submodule
+echo "Adding SEAL submodule..."
+git submodule add https://github.com/microsoft/SEAL.git SCI/extern/SEAL
+pushd SCI/extern/SEAL && git checkout 1d5c816 && popd
 
 # Add and initialize Eigen submodule if missing
-if [ ! -d "SCI/extern/eigen" ]; then
-    echo "Adding Eigen submodule..."
-    git submodule add https://gitlab.com/libeigen/eigen.git SCI/extern/eigen
-    pushd SCI/extern/eigen && git checkout 603e213 && popd
-else
-    echo "Eigen submodule already exists."
-fi
+echo "Adding Eigen submodule..."
+git submodule add https://gitlab.com/libeigen/eigen.git SCI/extern/eigen
+pushd SCI/extern/eigen && git checkout 603e213 && popd
 
 # Update all submodules recursively
 git submodule update --init --recursive
-
 
 mkdir -p build
 cd build
